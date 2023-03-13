9de91ed8be6e774950b308d139a947b199d86cc7
Lakshmana Teja Kapuganti <76482841+Lakshmanateja@users.noreply.github.com>
2023-03-13 21:19:58 +0530

    Viral Advertising

---
+++ Viral Advertising.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    count=0
+    m=5
+    for i in range(n):
+        f=m//2
+        count=count+f
+        m=f*3
+    return count    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
