ae24104ade66e0d539b44c305b8e80a4a5af233b
Lakshmana Teja Kapuganti <76482841+Lakshmanateja@users.noreply.github.com>
2023-03-18 19:32:30 +0530

    Service Lane

---
+++ Service Lane.py
@@ -0,0 +1,47 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'serviceLane' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY cases
+#
+
+def serviceLane(n, cases, width):
+    # Write your code here
+    list2=[]
+    for i in range(len(cases)):
+        list1=[]
+        for j in range(cases[i][0],cases[i][1]+1):
+            list1.append(width[j])
+        list2.append(min(list1))
+    return list2        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    width = list(map(int, input().rstrip().split()))
+
+    cases = []
+
+    for _ in range(t):
+        cases.append(list(map(int, input().rstrip().split())))
+
+    result = serviceLane(n, cases, width)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
